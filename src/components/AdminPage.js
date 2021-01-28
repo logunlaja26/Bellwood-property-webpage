5c94cdec23947797e5317829ad1c5f24f3af1e4b
logunlaja26 <logunlaja26@gmail.com>
2021-01-28 11:00:03 -0600

    added header component

---
+++ src/components/AdminPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class AdminPage extends Component {
+  render() {
+    return <div>Admin</div>;
+  }
+}
+
+export default AdminPage;
---
+++ src/components/MainPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Homepage from "./HomePage";
+import Adminpage from "./AdminPage";
+import Header from "./Header";
+import Footer from "./Footer";
+
+export class MainPage extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default MainPage;
